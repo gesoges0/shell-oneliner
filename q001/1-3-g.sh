43c8aa92b6a58bc21ed0035b3935efb347ee5271
gesoges0 <elasticnet1995@gmail.com>
2023-05-02 00:39:51 +0900

    add: 1-3-g

---
+++ q001/1-3-g.sh
@@ -0,0 +1,15 @@
+# 1
+seq 4 | awk '{print "mkdir " ($1%2 ? "odd_" : "even_") $1}'
+
+# mkdir odd_1
+# mkdir even_2
+# mkdir odd_3
+# mkdir even_4
+
+seq 4 | awk '{print "mkdir " ($1%2 ? "odd_": "even_") $1}' | bash
+
+seq 4 | awk '{print ($1%2 ? "odd_" : "even_")$1}' | xargs rmdir
+
+# make bash file
+seq 4 | awk '{print "mkdir " ($1%2 ? "odd_" : "even_") $1}' > a
+# bash ./a
