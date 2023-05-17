332ca49185ccb85f85773ae01480387a305bd866
gesoges0 <elasticnet1995@gmail.com>
2023-05-17 21:54:59 +0900

    add: 2-1-e

---
+++ p02/2-1-e.sh
@@ -0,0 +1,25 @@
+p=pen
+re=""
+
+# 1. 
+# - クォートなしは変数が解釈される
+# - ダブルクォートは変数が解釈される
+# - シングルクォートは解釈されない
+# pen pen $p
+echo $p "$p" '$p'
+
+# 2.
+# This is a pen. This is a $p
+echo "This is a $p." 'This is a $p'
+
+# 3.
+# This is a pencil. That is a pencil. That was a .
+echo "This is a ${p}cil." That is a "$p"cil. "That was a $pcil."
+
+# 4-1. 
+# grep "$re" /etc/passwd
+
+
+# 4-2.
+# grep /etc/passwd になってしまい止まる
+# grep $re /etc/passwd
